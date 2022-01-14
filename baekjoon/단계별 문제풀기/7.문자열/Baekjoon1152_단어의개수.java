9eba9a471b886474d3a99c8354bf9c8fddb517cd
이호준 <59757689+hojunnnnn@users.noreply.github.com>
2022-01-14 16:22:19 +0900

    .
    .

--- baekjoon/단계별 문제풀기/7.문자열/Baekjoon1152_단어의개수.java
+++ baekjoon/단계별 문제풀기/7.문자열/Baekjoon1152_단어의개수.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Baek1152 {
+public class Baekjoon1152_단어의개수 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
