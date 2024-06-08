7773433f9c804ee5650ec44f758f2aa2615f7f5b
HJLee <59757689+hojunnnnn@users.noreply.github.com>
2024-06-08 21:06:58 +0900

    240608_2

---
+++ src/programmers/etc/피자_나눠_먹기_2.java
@@ -0,0 +1,18 @@
+package programmers.etc;
+
+public class 피자_나눠_먹기_2 {
+    public static void main(String[] args) {
+        System.out.println(solution(6));
+        System.out.println(solution(10));
+        System.out.println(solution(4));
+        System.out.println(solution(1));
+    }
+
+    public static int solution(int n) {
+        int answer = 1;
+        while ((6 * answer) % n == 0) {
+            answer++;
+        }
+        return answer;
+    }
+}
